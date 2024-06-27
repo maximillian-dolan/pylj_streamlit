66beffe1e4a31e1176a67715fe159b9ecebda74b
maxdolan <maxdolan@MacBook-Pro-106.local>
2024-06-27 22:57:41 +0100

    rudimentary dashboard worksd

--- home.py
+++ home.py
@@ -3,14 +3,20 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import plotly.express as px
+import plotly.subplots as sp
+
+options = st.popover('options')
+
+with options:
+    number_of_particles = st.slider('Number of particles', min_value = 1, max_value = 50, step = 1, value = 20)
+    box_length = st.slider('Box Length', min_value = 5, max_value = 20, step = 1, value = 100)
+    number_of_steps = st.slider('Number of steps', min_value = 100, max_value = 5000, step = 100, value = 5000)
+    temperature = st.slider('Temperature', min_value = 0, max_value = 1500, step = 100, value = 1000)
 
 def md_simulation(number_of_particles, temperature, box_length, number_of_steps, sample_frequency):
     # Initialise the system
     system = md.initialise(number_of_particles, temperature, box_length, 'square')
     # This sets the sampling class
-
-    xpositions = []
-    ypositions = []
     positions = pd.DataFrame({})
     current_positions = {}
 
@@ -35,19 +41,70 @@ def md_simulation(number_of_particles, temperature, box_length, number_of_steps,
             current_positions['xposition'] = system.particles['xposition']
             current_positions['yposition'] = system.particles['yposition']
             current_positions['particle'] = np.arange(number_of_particles)
-            xpositions.append(system.particles['xposition'])
+            current_positions['velocity'] = np.sqrt(system.particles['xvelocity']**2 + system.particles['yvelocity']**2)
+            current_positions['energy'] = system.particles['energy']
             df = pd.DataFrame(current_positions)
             positions = pd.concat([positions, df], ignore_index=True)
 
-    return system, positions, xpositions
+    return system, positions
 
-system, positions, xpositions = md_simulation(20, 1000, 100, 5000, 100)
+if st.button('generate'):
+    system, positions = md_simulation(number_of_particles, temperature, box_length, number_of_steps, 100)
+else:
+    system, positions = md_simulation(20, 1000, 100, 100, 100)
 
-fig = px.scatter(positions,
+
+#=================================================
+#=================================================
+#Creating animation
+#=================================================
+animation = px.scatter(positions,
                  x = 'xposition',
                  y = 'yposition',
                  animation_frame = 'cycle',
-                 animation_group = 'particle'
+                 animation_group = 'particle',
+                 range_x = [0, box_length*1e-10],
+                 range_y = [0, box_length*1e-10]
                  )
 
-st.plotly_chart(fig)
+# Update animation axes to just be a box
+animation.update_yaxes(showgrid=False,
+                       #showline = True,
+                       #linewidth = 1,
+                       #mirror = True,
+                       zeroline = False,
+                       showticklabels=False,
+                       title_text = '')
+animation.update_xaxes(showgrid=False,
+                       #showline = True,
+                       #linewidth = 1,
+                       #mirror = True,
+                       zeroline = False,
+                       showticklabels=False,
+                       title_text = '')
+#=================================================
+#=================================================
+velocity_fig = px.bar(positions,
+                    x = 'particle',
+                    y = 'velocity',
+                    animation_frame = 'cycle',
+                    animation_group = 'particle',
+                    range_y = [0, max(positions['velocity'])]
+                    )
+
+energy_fig = px.bar(positions,
+                    x = 'particle',
+                    y = 'energy',
+                    animation_frame = 'cycle',
+                    animation_group = 'particle',
+                    range_y = [min(positions['energy']), max(positions['energy'])]
+                    )
+
+col1,col2 = st.columns(2)
+
+
+with col1:
+    st.plotly_chart(animation, use_container_width = True)
+
+with col2:
+    st.plotly_chart(velocity_fig, use_container_width = True)
